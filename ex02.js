51e340c7e7e2e9f557e856fc68f902609e92205d
Leandro Farias <leandrorf1606@gmail.com>
2025-05-08 22:47:02 -0300

    finalizado ex02

--- ex02.js
+++ ex02.js
@@ -1,21 +1,20 @@
 /*
   2. Escreva um programa que pergunte a velocidade de um carro. Caso ultrapasse 80 Kmh-1, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$ 5,00 por cada Km acima da velocidade permitida.
 */
-
 const prompt = require('prompt-sync')()
 
-let velocidade = parseInt(prompt("Sua velocidade? "))
+let velocidade =  parseInt(prompt("Sua velocidade? "))
 let multa = 0
 
 if (isNaN(velocidade)) return
 
 function checagem(vel) {
   if (vel > 80) {
-    for (let i = 80; i < vel; i++) {
-      multa += 5
-    }
+    multa = (vel - 80) * 5
+    console.log(`Você foi multado em ${multa.toLocaleString('pt-BR', {style: 'currency', currency: "BRL"})}`);
+  } else {
+    console.log('Dentro do limite');
   }
-console.log(`Você foi multado em ${multa.toLocaleString('pt-BR', {style: 'currency', currency: "BRL"})}`);
 }
 
 checagem(velocidade)
