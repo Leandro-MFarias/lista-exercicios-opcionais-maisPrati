894c16550bbc6ec27ab0b44a6e77972e67f41bbd
Leandro Farias <leandrorf1606@gmail.com>
2025-05-24 16:26:14 -0300

    ex08-feito

--- ex08.js
+++ ex08.js
@@ -9,4 +9,23 @@
   Faça um programa que leia quantas horas de atividade uma pessoa teve por mês.
   Calcule e mostre quantos pontos ela teve e quanto dinheiro ela conseguiu ganhar.
 */
+const prompt = require('prompt-sync')()
 
+let horas = parseInt(prompt("Quantas horas de atividade você fez no mês: "))
+if (isNaN(horas)) return console.log("Digite um numero valido");
+if (horas === 0) return console.log("Zero pontos você não ganhou nada esse mês!");
+
+let troca = 0.05
+let pontos = 0
+
+if (horas <= 10) {
+  pontos = horas * 2;
+} else if (horas <= 20) {
+  pontos = horas * 5;
+} else {
+  pontos = horas * 10;
+}
+
+let ganhoTotal = pontos * troca
+
+console.log(`Você fez atividade por ${horas}, recebeu: ${pontos} e ganhou um total de: ${ganhoTotal.toLocaleString("pt-BR", { style: "currency", currency: 'BRL' })}`);
